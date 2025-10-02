31fa3d9950517ff42de8c7e1ec3cb8e2b36c1207
최진옥 <dkdldb5044@naver.com>
2025-10-02 18:07:48 +0900

    2일차 과제

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,42 @@
-// App.js
 import { useState } from "react";
-
-import "./App.css";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 
-//1. 아래 todos 상태의 초기값 구조를 보고 Todo 타입을 정의하세요.
 export interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 
+export enum Filter {
+  SHOW_ALL = "모든 할 일 보기",
+  HIDE_COMPLETED = "완료된 할 일 숨기기",
 }
-//2. showCompleted의 상태를 열거형으로 저장하세요.
-//- 들어갈 내용은 다음과같습니다.
-//- SHOW_ALL = "모든 할 일 보기",
-//- HIDE_COMPLETED = "완료된 할 일 숨기기",
-enum Filter {
-  
-}
+
 const App = () => {
-  //3. todo 상태가 올바른 타입을 받아올 수 있도록 타입을 지정해주세요.
-  //힌트 : 리액트에서 props의 타입을 정의하는 방법 -> useState<타입>(초기값)
-  const [todos, setTodos] = useState<>([
+  const [todos, setTodos] = useState<Todo[]>([
     { id: 1, text: "할 일 1", completed: false },
     { id: 2, text: "할 일 2", completed: false },
     { id: 3, text: "할 일 3", completed: true },
   ]);
-  //4. filter 상태가 올바른 타입을 받아올 수 있도록 타입을 지정해주세요.
-  //힌트 : 리액트에서 state의 타입을 정의하는 방법 -> useState<타입>(초기값)
-  //이때 초기값은 Filter.SHOW_ALL로 설정합니다.
-  const [filter, setFilter] = useState();
 
-  //5. switch문을 사용해서 filter상태를 업데이트하세요.
-  //filter상태가 SHOW_ALL일 경우, HIDE_COMPLETED 로 업데이트합니다.
-  //filter상태가 HIDE_COMPLETED일 경우, SHOW_ALL 로 업데이트합니다.
-  //다른 방법으로 구현할 수도 있지만 강의에 나왔던 'enum타입을 switch문으로 활용하는 코드'를 직접 작성해봅시다!
+  const [filter, setFilter] = useState<Filter>(Filter.SHOW_ALL);
+
   const toggleFilter = () => {
     switch (filter) {
-      case 
-      //코드를 작성하세요
+      case Filter.SHOW_ALL:
+        setFilter(Filter.HIDE_COMPLETED);
         break;
-      case 
-      //코드를 작성하세요
+      case Filter.HIDE_COMPLETED:
+        setFilter(Filter.SHOW_ALL);
         break;
       default:
         setFilter(Filter.SHOW_ALL);
     }
   };
-  //6. addTodo의 매개변수 text의 타입을 지정해주세요. Todo 타입을 참고하세요.
-  const addTodo = (text) => {
-    const newTodo = {
+
+  const addTodo = (text: string) => {
+    const newTodo: Todo = {
       id: Date.now(),
       text,
       completed: false,
@@ -55,8 +44,7 @@ const App = () => {
     setTodos([...todos, newTodo]);
   };
 
-  //7. toggleComplete의 매개변수 id의 타입을 지정해주세요. Todo 타입을 참고하세요
-  const toggleComplete = (id) => {
+  const toggleComplete = (id: number) => {
     setTodos(
       todos.map((todo) =>
         todo.id === id ? { ...todo, completed: !todo.completed } : todo
